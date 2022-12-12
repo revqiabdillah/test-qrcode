630b14c66e3db7659abfb2885293d087db86e28b
revqiabdillah <rifqi.abdillah@sirclo.com>
2022-12-12 15:11:10 +0700

    fixing 3

--- pages/scane6.js
+++ pages/scane6.js
@@ -6,14 +6,18 @@ function Scan6() {
 
   return (
     <>
-      <BarcodeScannerComponent
-        width={500}
-        height={500}
-        onUpdate={(err, result) => {
-          if (result) setData(result.text);
-          else setData("Not Found");
-        }}
-      />
+      {
+        typeof window !== 'undefined' && (
+            <BarcodeScannerComponent
+                width={500}
+                height={500}
+                onUpdate={(err, result) => {
+                if (result) setData(result.text);
+                else setData("Not Found");
+                }}
+            />
+        )
+      }
       <p>{data}</p>
     </>
   );
